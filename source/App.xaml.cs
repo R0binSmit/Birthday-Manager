c5ac2ba2d2330c909f38d16d2d76e1b2714e9459
Robin Smit <Nyxero@web.de>
2022-08-16 19:34:02 +0200

    Init maui project files

---
+++ source/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Birthday_Manager;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ source/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Birthday_Manager;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
